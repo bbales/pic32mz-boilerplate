123abf58140b207139f0d884a316b3222049ca98
bbales <ben.bales.dev@gmail.com>
2016-09-30 21:59:34 -0400

    refactored ISRs

--- audio.c
+++ audio.c
@@ -14,7 +14,9 @@
 unsigned int sinTable[48] = {0, 280302863, 555809666, 821806412, 1073741823, 1307305213, 1518500249, 1703713324, 1859775392, 1984016187, 2074309916, 2129111626, 2147483647, 2129111626, 2074309916, 1984016187, 1859775392, 1703713324, 1518500249, 1307305213, 1073741823, 821806412, 555809666, 280302863, 0, -280302864, -555809667, -821806413, -1073741824, -1307305214, -1518500250, -1703713325, -1859775393, -1984016188, -2074309917, -2129111627, -2147483647, -2129111627, -2074309917, -1984016188, -1859775393, -1703713325, -1518500250, -1307305214, -1073741824, -821806413, -555809667, -280302864};
 int sinCount = 0;
 
+
 void codecEnable(int enable){
+    // RST pin on CS4272
     TRISCbits.TRISC14 = OUTPUT;
     if(enable){
         delay_ms(3);
@@ -29,7 +31,7 @@ void codecInit() {
     codecEnable(0);
 
     // Setup reference clocks for MCLK
-    initReferenceClocks();
+    codecInitMCLK();
 
     // Initialize channel samples
     left_input = 0;
@@ -44,6 +46,10 @@ void codecInit() {
     I2C1CONbits.ON = 0; // Disable SCL1
     I2C5CONbits.ON = 0; // Disable SDA5
     PMCONbits.ON = 0;   // Disable PMA9
+
+    //
+    // Set up pin IO
+    //
 
     // CODEC SDO (PIC SDI @ RD11)
     TRISDbits.TRISD11 = INPUT;
@@ -62,54 +68,43 @@ void codecInit() {
 
     // CODEC MCLK Output
     TRISFbits.TRISF5 = OUTPUT;
-    LATFbits.LATF5 = 0;
     RPF5R = 0b1111;
 
-    // SPI4 Setup
-    SPI4CONbits.ON = 0; // Turn off
+    //
+    // Set up SPI4 for I2S
+    //
+
+    // Baud Rate - PBCLK / SPIBRG
+    SPI4BRG = 0b000000001; // 64 * Fs (384x Mode)  === 6,139,089 Hz
+    SPI4BUF = 0; // Clear Buffer
+
+    // Turn off SPI4 for config
+    SPI4CONbits.ON = 0;
 
     // Enable codec support
     SPI4CON2bits.AUDEN = 1;
 
-    // Audio Protocol Mode
-    //   11 = PCM/DSP
-    //   10 = Right Justified
-    //   01 = Left Justified
-    //   00 = I2S
+    // Audio Protocol Mode (I2S)
     SPI4CON2bits.AUDMOD = 0b00;
 
-    // PBCLK / SPIBRG
-    SPI4BRG = 0b000000001; // 64 * Fs (384x Mode)  === 6,139,089 Hz
-    SPI4BUF = 0; // Clear Buffer
-
-    SPI4STATbits.SPIROV = 0;
-
+    // Important config
     SPI4CONbits.FRMEN = 0; // Framed mode disabled
     SPI4CONbits.MSSEN = 0; // Master mode SS enable
     SPI4CONbits.ENHBUF = 0; // Enhanced buffer mode 0*
     SPI4CONbits.MCLKSEL = 1; // Master Clk Select, 1 = REFCLKO1, 0 = PBCLK2
     SPI4CONbits.DISSDO = 0; // Disable SDO, 1 = SDO not used, 0 = SDO is used
     SPI4CONbits.DISSDI = 0; // Disable SDI, 1 = SDI not used, 0 = SDI is used
-
-    // 23/16 Bit communication select bits
-    //  When AUDEN = 1
-    //   32 16 communication
-    //   1  1  24 bit data, 32 bit fifo, 32 bit channel/64 bit frame
-    //   1  0  32 bit data, 32 bit fifo, 32 bit channel/64 bit frame
-    //   0  1  16 bit data, 16 bit fifo, 32 bit channel/64 bit frame
-    //   0  0  16 bit data, 16 bit fifo, 16 bit channel/32 bit frame
-    SPI4CONbits.MODE16 = 1; // 24 bit data
-    SPI4CONbits.MODE32 = 1; // 24 bit data
-
     SPI4CONbits.SMP = 0; // Input sample phase, 1* = end, 0 = middle
     SPI4CONbits.CKE = 1; // Clock edge select, 1 = data changes on active->idle, 0 = data changes on idle->active
     SPI4CONbits.CKP = 1; // Clock polarity select, 1 = active is low, 0 = active is high
     SPI4CONbits.MSTEN = 1; // Master Mode Enable, 1 = master, 0 = slave
     SPI4CONbits.SRXISEL = 0b11; // Recieve buffer interrupt is generated when buffer is full (pg 310)
 
-    SPI4CON2bits.AUDMONO = 0; // Audio is stereo
+    // Configure for 24-bit data in 32-bit frames
+    SPI4CONbits.MODE16 = 1;
+    SPI4CONbits.MODE32 = 1;
 
-    // Sign Extend Read data from RX FIFO bit 0*
+    // Sign Extend Read data from RX FIFO bit 0 (This may be useful)
     SPI4CON2bits.SPISGNEXT = 0;
 
     // Ignore overflow and dont generate error events
@@ -118,6 +113,11 @@ void codecInit() {
     SPI4CON2bits.SPIROVEN = 0; // Enable overglow error interrupts
     SPI4CON2bits.SPITUREN = 0; // Enable transmit underrun error interrupts
     SPI4CON2bits.FRMERREN = 0; // Enable frame error interrupts
+    SPI4STATbits.SPIROV = 0; // Reset overflow stat
+
+    //
+    // Configure SPI4 interrupts
+    //
 
     // Disable SPI4 Interrupts
     asm volatile("di");
@@ -150,7 +150,7 @@ void codecInit() {
     codecEnable(1);
 }
 
-void __ISR_AT_VECTOR(_SPI4_RX_VECTOR,IPL7SRS) rwCodec2(void){
+void codecRW(){
     if(channel){
         sinCount+=1;
         if(sinCount == 48) sinCount = 0;
@@ -204,7 +204,7 @@ void __ISR_AT_VECTOR(_SPI4_RX_VECTOR,IPL7SRS) rwCodec2(void){
     IFS5bits.SPI4RXIF = 0;
 }
 
-void initReferenceClocks() {
+void codecInitMCLK() {
     // REFCLKO1 for ADC
     REFO1CONbits.ON = 0;
     REFO1CONbits.ACTIVE = 0;
@@ -214,26 +214,3 @@ void initReferenceClocks() {
     REFO1TRIMbits.ROTRIM = 0b000100011;
     REFO1CONbits.ON = 1;
 }
-
-// void rwCodec(){
-//     // Write to SPI4BUF
-//     SPI4BUF = left_output;
-//
-//     while(!IFS5bits.SPI4RXIF);
-//
-//     // Read SPI4BUF
-//     right_input = SPI4BUF;
-//
-//     // Clear Interrupt Flag
-//     IFS5bits.SPI4RXIF = 0;
-//     // Write to SPI4BUF
-//     SPI4BUF = right_output;
-//
-//     while(!IFS5bits.SPI4RXIF);
-//
-//     // Read SPI4BUF
-//     left_input = SPI4BUF;
-//
-//     // Clear Interrupt Flag
-//     IFS5bits.SPI4RXIF = 0;
-// }
--- audio.h
+++ audio.h
@@ -4,11 +4,16 @@
 #define OUTPUT 0
 #define INPUT 1
 
-void rwCodec();
-void initReferenceClocks();
+// CODEC protos
+void codecInitMCLK();
 void codecInit();
 void codecEnable(int enable);
+void codecRW();
 
+// Main CODEC interrupt subroutine
+void __ISR_AT_VECTOR(_SPI4_RX_VECTOR,IPL7SRS) codecRW(void);
+
+// Audio and sample variables
 int channel;
 signed long left_input;
 signed long right_input;
